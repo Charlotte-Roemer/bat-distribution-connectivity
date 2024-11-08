190b29a15bc7f25b573fd2ac9eceac2da0f84ec2
Thomas Sévère <thomas.severe@proton.me>
2024-11-08 21:47:36 +0100

    adding all scripts

---
+++ f_Coord/f_Coord_Meteo.R
@@ -0,0 +1,42 @@
+print("load Meteo")
+# points = FCoord
+library(dplyr)
+library(sf)
+Coord_Meteo <- function(points) {
+  if (opt$mode == "predict") {
+    mode <- "predict"
+    command <- paste0("python3 get_meteo.py --mode ", mode, " --date ", date_pred, " --file ", points, ".csv")
+  } else {
+    mode <- "train"
+    command <- paste0("python3 get_meteo.py --mode ", mode, " --file ", points, ".csv")
+  }
+
+  setwd("./prep_data_chiros/scripts")
+
+  system(command)
+
+  setwd(loc)
+
+  if (exists("date_pred")) {
+    OccSL <- readr::read_delim(paste0(points, ".csv"), delim = ",") %>%
+      dplyr::select(c("X", "Y"))
+    OccSL$FID <- c(1:nrow(OccSL))
+    OccSL <- OccSL %>%
+      sf::st_as_sf(coords = c("X", "Y"), crs = 4326, remove = FALSE)
+
+    OccSL_2000_meteo <- readr::read_delim(paste0(points_2000, "_meteo.csv"), delim = ",")
+    colnames(OccSL_2000_meteo)[1] <- "FID"
+
+    OccSL_2000_meteo$FID <- OccSL_2000_meteo + 1
+    OccSL_2000_meteo <- OccSL_2000_meteo %>%
+      sf::st_as_sf(coords = c("X", "Y"), crs = 4326, remove = FALSE)
+
+    OccSL_500_meteo <- sf::st_join(OccSL, OccSL_2000_meteo, st_nearest_feature,
+      left = TRUE, suffix = c("", ".y")
+    ) %>%
+      sf::st_drop_geometry() %>%
+      dplyr::select(-c(ends_with(".y"), FID))
+
+    data.table::fwrite(OccSL_500_meteo, paste0(points, "_meteo.csv"))
+  }
+}
---
+++ f_Coord/f_Coord_ROUTE500.R
@@ -0,0 +1,310 @@
+print("load routes500")
+
+Coord_Route <- function(points, names_coord, bs, bm, bl, folder) {
+  library(sf)
+  library(data.table)
+  library(tidyverse)
+
+  Sys.time()
+  route_file <- list.files(folder,
+    pattern = "TRONCON_ROUTE.shp",
+    recursive = TRUE, full.names = TRUE
+  )
+  ROUTE <- read_sf(route_file)
+
+  fer_file <- list.files(folder,
+    pattern = "TRONCON_VOIE_FERREE.shp",
+    recursive = TRUE, full.names = TRUE
+  )
+  FER <- read_sf(fer_file)
+
+  Sys.time()
+
+  #  # pour tests
+  # points <- FCoord
+
+  # names_coord  <- Coord_Headers
+  # bs <- BS
+  # bm <- BM
+  # bl <- BL
+  # fin variable test
+  FOccSL <- points
+  if (opt$mode == "predict") {
+    OccSL <- read.csv(paste0(points, ".csv")) %>%
+      dplyr::select(c("X", "Y"))
+
+    OccSL$FID <- c(1:nrow(OccSL))
+    OccSL <- OccSL %>%
+      st_as_sf(coords = c("X", "Y"), crs = 4326, remove = FALSE)
+
+    OccSL_L93 <- OccSL %>%
+      st_transform(2154)
+    OccSL_L93$Nuit <- date_pred
+
+    CoordH <- names_coord
+  } else {
+    OccSL <- read.csv(paste0(points, ".csv")) %>%
+      select(names_coord)
+    OccSL$FID <- c(1:nrow(OccSL))
+    OccSL <- OccSL %>%
+      st_as_sf(coords = c("X", "Y"), crs = 4326, remove = FALSE)
+
+    OccSL_L93 <- OccSL %>%
+      st_transform(2154)
+    CoordH <- names_coord
+  }
+
+  BufferSmall <- bs
+  BufferMedium <- bm
+  BufferLarge <- bl
+
+
+  ##########################################
+  ##########################################
+  #############  Routes    #################
+  ##########################################
+  ##########################################
+
+  # Write dictionary
+  ClassP <- unique(ROUTE$VOCATION)
+  ClassP <- ClassP[order(ClassP)]
+  CPd <- data.frame(ClassP, Code = c(1:length(ClassP)))
+  fwrite(CPd, "ROUTE500_dictionary.csv", sep = ";")
+
+  OccSL_L93Re <- OccSL_L93
+  for (h in 1:length(ClassP))
+  {
+    ROUTEP <- ROUTE[ROUTE$VOCATION == ClassP[h], ]
+    print(ClassP[h])
+    print(names(OccSL_L93Re))
+    print(h)
+
+    ########
+    # Buffer S
+    ########
+
+    BufferS <- st_buffer(OccSL_L93, dist = BufferSmall) %>%
+      st_transform(st_crs(ROUTE))
+
+    Sys.time()
+    BufferS$Route_count <- st_intersects(BufferS, ROUTEP) %>%
+      lengths()
+    Sys.time()
+
+    # library(viridis)
+    # BufferS %>%
+    #   st_crop(xmin=161290, xmax=211290 , ymin=6046796 , ymax=7109796) %>% #zoom in some area
+    #   ggplot( aes(fill=Route_count)) +
+    #   geom_sf() +
+    #   scale_fill_gradientn(colours=rev(magma(6)))
+
+    SpRoute <- BufferS
+
+    if (is.null(BufferS$Route_count)) {
+      OccSL_L93Re$SpRo_S <- 0
+    } else {
+      PC_50 <- aggregate(SpRoute$Route_count, by = list(SpRoute$FID), FUN = sum)
+      names(PC_50)[ncol(PC_50)] <- "SpRo_S"
+      OccSL_L93Re <- merge(OccSL_L93Re, PC_50, by.x = "FID", by.y = "Group.1", all.x = T)
+      OccSL_L93Re$SpRo_S[is.na(OccSL_L93Re$SpRo_S)] <- 0
+      # spplot(OccSL_L93Re,zcol="SpRo_S",col="transparent")
+    }
+    names(OccSL_L93Re)[names(OccSL_L93Re) == "SpRo_S"] <- paste0("SpRo", h, "S")
+
+    ########
+    # Buffer M
+    ########
+
+    BufferM <- st_buffer(OccSL_L93, dist = BufferMedium) %>%
+      st_transform(st_crs(ROUTE))
+
+    Sys.time()
+    BufferM$Route_count <- st_intersects(BufferM, ROUTEP) %>%
+      lengths()
+    Sys.time()
+
+    # library(viridis)
+    # BufferM %>%
+    #   st_crop(xmin=161290, xmax=211290 , ymin=6046796 , ymax=7109796) %>% #zoom in some area
+    #   ggplot( aes(fill=Route_count)) +
+    #   geom_sf() +
+    #   scale_fill_gradientn(colours=rev(magma(6)))
+
+    SpRoute <- BufferM
+
+    if (is.null(BufferM$Route_count)) {
+      OccSL_L93Re$SpRo_M <- 0
+    } else {
+      PC_50 <- aggregate(SpRoute$Route_count, by = list(SpRoute$FID), FUN = sum)
+      names(PC_50)[ncol(PC_50)] <- "SpRo_M"
+      OccSL_L93Re <- merge(OccSL_L93Re, PC_50, by.x = "FID", by.y = "Group.1", all.x = T)
+      OccSL_L93Re$SpRo_M[is.na(OccSL_L93Re$SpRo_M)] <- 0
+      # spplot(OccSL_L93Re,zcol="SpRo_S",col="transparent")
+    }
+    names(OccSL_L93Re)[names(OccSL_L93Re) == "SpRo_M"] <- paste0("SpRo", h, "M")
+
+
+    ########
+    # Buffer L
+    ########
+
+    BufferL <- st_buffer(OccSL_L93, dist = BufferLarge) %>%
+      st_transform(st_crs(ROUTE))
+
+    Sys.time()
+    BufferL$Route_count <- st_intersects(BufferL, ROUTEP) %>%
+      lengths()
+    Sys.time()
+
+    # library(viridis)
+    # BufferL %>%
+    #   st_crop(xmin=161290, xmax=211290 , ymin=6046796 , ymax=7109796) %>% #zoom in some area
+    #   ggplot( aes(fill=Route_count)) +
+    #   geom_sf() +
+    #   scale_fill_gradientn(colours=rev(magma(6)))
+
+    SpRoute <- BufferL
+
+    if (is.null(BufferL$Route_count)) {
+      OccSL_L93Re$SpRo_L <- 0
+    } else {
+      PC_50 <- aggregate(SpRoute$Route_count, by = list(SpRoute$FID), FUN = sum)
+      names(PC_50)[ncol(PC_50)] <- "SpRo_L"
+      OccSL_L93Re <- merge(OccSL_L93Re, PC_50, by.x = "FID", by.y = "Group.1", all.x = T)
+      OccSL_L93Re$SpRo_L[is.na(OccSL_L93Re$SpRo_L)] <- 0
+      # spplot(OccSL_L93Re,zcol="SpRo_S",col="transparent")
+    }
+    names(OccSL_L93Re)[names(OccSL_L93Re) == "SpRo_L"] <- paste0("SpRo", h, "L")
+  }
+
+  ##########################################
+  ##########################################
+  ########## Voies ferr�es  #################
+  ##########################################
+  ##########################################
+
+
+  ########
+  # Buffer S
+  ########
+
+  print("FerS")
+
+  BufferS <- st_buffer(OccSL_L93, dist = BufferSmall) %>%
+    st_transform(st_crs(FER))
+
+  Sys.time()
+  BufferS$Fer_count <- st_intersects(BufferS, FER) %>%
+    lengths()
+  Sys.time()
+
+
+  SpFer <- BufferS
+
+  if (is.null(BufferS$Fer_count)) {
+    OccSL_L93Re$SpFe_S <- 0
+  } else {
+    PC_50 <- aggregate(SpFer$Fer_count, by = list(SpFer$FID), FUN = sum)
+    names(PC_50)[ncol(PC_50)] <- "SpFe_S"
+    OccSL_L93Re <- merge(OccSL_L93Re, PC_50, by.x = "FID", by.y = "Group.1", all.x = T)
+    OccSL_L93Re$SpFe_S[is.na(OccSL_L93Re$SpFe_S)] <- 0
+    # spplot(OccSL_L93Re,zcol="SpFe_S",col="transparent")
+  }
+
+  ########
+  # Buffer M
+  ########
+
+  print("FerM")
+
+  BufferM <- st_buffer(OccSL_L93, dist = BufferMedium) %>%
+    st_transform(st_crs(FER))
+
+  Sys.time()
+  BufferM$Fer_count <- st_intersects(BufferM, FER) %>%
+    lengths()
+  Sys.time()
+
+  # library(viridis)
+  # BufferM %>%
+  #   st_crop(xmin=161290, xmax=211290 , ymin=6046796 , ymax=7109796) %>% #zoom in some area
+  #   ggplot( aes(fill=Fer_count)) +
+  #   geom_sf() +
+  #   scale_fill_gradientn(colours=rev(magma(6)))
+
+  SpFer <- BufferM
+
+  if (is.null(BufferM$Fer_count)) {
+    OccSL_L93Re$SpFe_M <- 0
+  } else {
+    PC_50 <- aggregate(SpFer$Fer_count, by = list(SpFer$FID), FUN = sum)
+    names(PC_50)[ncol(PC_50)] <- "SpFe_M"
+    OccSL_L93Re <- merge(OccSL_L93Re, PC_50, by.x = "FID", by.y = "Group.1", all.x = T)
+    OccSL_L93Re$SpFe_M[is.na(OccSL_L93Re$SpFe_M)] <- 0
+    # spplot(OccSL_L93Re,zcol="SpFe_M",col="transparent")
+  }
+
+  ########
+  # Buffer L
+  ########
+
+  print("FerL")
+
+  BufferL <- st_buffer(OccSL_L93, dist = BufferLarge) %>%
+    st_transform(st_crs(FER))
+
+  Sys.time()
+  BufferL$Fer_count <- st_intersects(BufferL, FER) %>%
+    lengths()
+  Sys.time()
+
+  # library(viridis)
+  # BufferL %>%
+  #   st_crop(xmin=161290, xmax=211290 , ymin=6046796 , ymax=7109796) %>% #zoom in some area
+  #   ggplot( aes(fill=Fer_count)) +
+  #   geom_sf() +
+  #   scale_fill_gradientn(colours=rev(magma(6)))
+
+  SpFer <- BufferL
+
+  if (is.null(BufferL$Fer_count)) {
+    OccSL_L93Re$SpFe_L <- 0
+  } else {
+    PC_50 <- aggregate(SpFer$Fer_count, by = list(SpFer$FID), FUN = sum)
+    names(PC_50)[ncol(PC_50)] <- "SpFe_L"
+    OccSL_L93Re <- merge(OccSL_L93Re, PC_50, by.x = "FID", by.y = "Group.1", all.x = T)
+    OccSL_L93Re$SpFe_L[is.na(OccSL_L93Re$SpFe_L)] <- 0
+    # spplot(OccSL_L93Re,zcol="SpFe_L",col="transparent")
+  }
+
+  ##########################################
+  ##########################################
+  #############  Reseaux   #################
+  ##########################################
+  ##########################################
+
+  #
+  OccSL_WGS84 <- OccSL_L93 %>%
+    st_transform(4326) # back transform to WGS84
+
+  OccSL_ARajouter <- subset(OccSL_L93Re, select = grepl("Sp", names(OccSL_L93Re)))
+
+  Reseau <- data.frame(cbind(
+    st_coordinates(OccSL_WGS84),
+    as.data.frame(OccSL_ARajouter)
+  ))
+
+  Reseau <- Reseau %>%
+    st_drop_geometry() %>%
+    select(!geometry)
+
+
+  NewName <- paste0(FOccSL, "_Transports.csv")
+  fwrite(Reseau, NewName)
+
+  # coordinates(Reseau) <- CoordH
+
+  # SelCol=sample(names(OccSL_ARajouter),1)
+  # spplot(Reseau,zcol=SelCol,main=SelCol)
+  # class(Reseau)
+}
