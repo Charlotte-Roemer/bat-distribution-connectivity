88a5954899a61f28b886ab5b51b16ea11076bf41
Thomas Sévère <thomas.severe@proton.me>
2024-11-13 17:08:20 +0100

    managed regular 500m grid/fortnight code

--- PCIA_Pipeline_GIS.R
+++ PCIA_Pipeline_GIS.R
@@ -3,6 +3,7 @@ library(exactextractr)
 library(svDialogs)
 library(optparse)
 library(dplyr)
+library(stringr)
 library(sf)
 source("variables.R")
 
@@ -11,56 +12,79 @@ source("variables.R")
 setwd(project_path) # dossier dans lequel
 
 option_list <- list(
-  optparse::make_option(c("-m", "--mode"), type = "character", default = NULL,
-    help = "Set train or predict mode"), 
-  optparse::make_option(c("-r", "--region"), type = "character", default = "france_met",
-    help = 'Set region of interest between "france_met" (default), "europe", "idf"'),
-  optparse::make_option(c("-d", "--date"), type = "character", default = NULL,
-    help = 'Set fortnight date for predict grid extraction.'),
-  optparse::make_option(c("-s", "--size"), type = "integer", default = 500,
-    help = 'Set grid size in meters.'),
-  optparse::make_option(c("-f", "--force-all"), type = "logical", default = FALSE,
-    help = 'Force recreating all variables (TRUE/FALSE)')
+  optparse::make_option(c("-m", "--mode"),
+    type = "character", default = NULL,
+    help = "Set train or predict mode"
+  ),
+  optparse::make_option(c("-r", "--region"),
+    type = "character", default = "france_met",
+    help = 'Set region of interest between "france_met" (default), "europe", "idf"'
+  ),
+  optparse::make_option(c("-d", "--date"),
+    type = "character", default = NULL,
+    help = "Set fortnight date for predict grid extraction."
+  ),
+  optparse::make_option(c("-s", "--size"),
+    type = "integer", default = 500,
+    help = "Set grid size in meters."
+  ),
+  optparse::make_option(c("-f", "--force-all"),
+    type = "logical", default = FALSE,
+    help = "Force recreating all variables (TRUE/FALSE)"
+  )
 )
 
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
-if (is.null(opt$region)){
+if (is.null(opt$region)) {
   print_help(opt_parser)
   stop("At least one region must be supplied from france_met, europe, idf with argument --region")
 }
 
-data_folder  <- data_path
+data_folder <- data_path
 zone_file <- file.path(data_folder, "GIS", "regions.gpkg")
+dir.create(file.path(data_folder, "observations", "obs_vars"))
+dir.create(file.path(data_folder, "observations", "pred_vars"))
+
+obs_vars_folder <- file.path(data_folder, "observations", "obs_vars")
+pred_vars_folder <- file.path(data_folder, "observations", "pred_vars")
+
+# I keep the loop for later (meteo) no need for folder but useful as filenaming
+# for (i in seq(3, 11)){
+# for (ii in seq(1, 2)){
+# folder_name <- paste0(sprintf("%02d", i), "-", ii)
+# dir.create(file.path(pred_vars_folder, folder_name), showWarnings = FALSE)
+# }
+# }
 
 zone <- st_read(zone_file, layer = "france_met")
 
+
 # Localisation des fonctions d’extraction (relative à loc) :
-folderfun <- file.path(project_path, "scripts", "f_Coord")
+folderfun <- file.path(project_path, "f_Coord")
 
 
 # FCoord varie selon qu’on soit sur les observations ou de la préparation
 # de données de prédiction
 
-#valid <- FALSE
-#train <- TRUE  #Si on veut entrainer le modèle, sinon predict
+# valid <- FALSE
+# train <- TRUE  #Si on veut entrainer le modèle, sinon predict
 
 # replace date setting with options
 # to be replaced
 if (opt$mode == "predict") {
   # date_pred should be in fortnightnumber_year format fn_yyyy. Fortnight scales
- # from 1 to 24 as there are two every month (ex : 2_2023)
+  # from 1 to 24 as there are two every month (ex : 2_2023)
   date_pred <- opt$date
-
 }
 # end of replacement
 
 
-#Pipeline <- "exists"
+# Pipeline <- "exists"
 
 if (opt$mode == "train") {
-  nuits_obs_file <- file.path(data_folder,"observations",  "parti_unique_non_confi.csv")
+  nuits_obs_file <- file.path(data_folder, "observations", "parti_unique_non_confi.csv")
   nuits_obs <- read.csv2(nuits_obs_file)
   nuits_obs$X <- nuits_obs$longitude
   nuits_obs$Y <- nuits_obs$latitude
@@ -68,50 +92,58 @@ if (opt$mode == "train") {
   locs <- sf::st_intersection(locs$geometry, zone)
   locs$FID <- 1:nrow(locs)
   # setting the fortnight number (1-24) :
-  locs$fortnight <- ifelse(as.integer(format(as.Date(locs$Nuit), "%d")) <= 15, 
+  locs$fortnight <- ifelse(as.integer(format(as.Date(locs$Nuit), "%d")) <= 15,
     as.integer(format(as.Date(locs$Nuit), "%m")) * 2 - 1,
-    as.integer(format(as.Date(locs$Nuit), "%m")) *2)
+    as.integer(format(as.Date(locs$Nuit), "%m")) * 2
+  )
   locs$fortnight_year <- paste0(locs$fortnight, "_", format(as.Date(locs$Nuit), "%Y"))
 
   locs_l93 <- locs %>% sf::st_transform(2154)
-  dates <- unique(locs_l93$Nuit)
-  codes <- list()
 
-#  # If we want to group local observations by fortnight rather than night :
-  #dates <- unique(locs_l93$fortnight_year) # or locs_l93$fortnight to lose year effect
-  #codes <- list()
+  grid_file <- file.path(data_folder, "GIS", paste0("SysGrid_500m_de_cote_", opt$region, ".csv"))
+  study_area <- st_read(zone_file, layer = opt$region)
+  study_area_m <- st_transform(study_area, 2154)
+  study_area_m_buf <- st_buffer(study_area_m, 250)
 
-  #for (date in dates){
-    #subset_fortnight <- locs_l93[locs_l93$fortnight_year == date, ]
-    #buffer <- subset_fortnight %>% sf::st_buffer(250) %>% sf::st_union() %>% sf::st_cast('POLYGON')
-    #buffer <- sf::st_as_sf(buffer)
-    #buffer$code <- paste0(date,"_", 1:nrow(buffer))
-    #subset_fortnight <- subset_fortnight %>% sf::st_join(buffer, left = TRUE)
-    #codes <- rlist::list.append(codes, subset_fortnight)
-  #}
+  xmin <- st_bbox(study_area_m_buf)["xmin"] - 250
+  ymin <- st_bbox(study_area_m_buf)["ymin"] - 250
+  xmax <- st_bbox(study_area_m_buf)["xmax"] + 250
+  ymax <- st_bbox(study_area_m_buf)["ymax"] + 250
 
 
-  for (date in dates){
-    subset_nuit <- locs_l93[locs_l93$Nuit == date, ]
-    sbuffer <- subset_nuit %>% sf::st_buffer(250) %>% sf::st_union() %>% sf::st_cast('POLYGON')s
-    buffer <- sf::st_as_sf(buffer)
-    buffer$code <- paste0(date, 1:nrow(buffer))
-    subset_nuit <- subset_nuit %>% sf::st_join(buffer, left = TRUE)
-    codes <- rlist::list.append(codes, subset_nuit)
-  }
+  cols <- (xmax - xmin) / 500
+  rows <- (ymax - ymin) / 500
 
-  code_bind <- do.call("rbind", codes)
-  codes <- code_bind %>% dplyr::select(FID, code)
+  grid_polyg <- st_make_grid(study_area_m_buf,
+    cellsize = c(500, 500),
+    offset = c(xmin, ymin),
+    n = c(cols, rows)
+  ) %>% st_as_sf()
+
+  grid_polyg$grid_id <- seq(1, nrow(grid_polyg))
+
+  locs_l93 <- st_join(locs_l93, grid_polyg, join = st_covered_by)
+  width <- nchar(as.character(max(locs_l93$grid_id)))
+  locs_l93$code <- paste0(
+    str_pad(locs_l93$grid_id, width = width, pad = "0"),
+    "-",
+    locs_l93$fortnight
+  )
+
+
+  codes <- locs_l93 %>% dplyr::select(FID, code)
   codes <- codes %>% sf::st_drop_geometry()
   locs <- locs %>% dplyr::left_join(codes, by = "FID")
-  locs  <- locs %>% dplyr::select(-FID) %>% sf::st_drop_geometry()
+  locs <- locs %>%
+    dplyr::select(-FID) %>%
+    sf::st_drop_geometry()
   locs$X <- locs$longitude
   locs$Y <- locs$latitude
 
-  FCoord <- file.path(data_path, "variables", paste0("loc_train_", opt$region))
+  FCoord <- file.path(obs_vars_folder, paste0("loc_train_", opt$region))
   readr::write_delim(locs, paste0(FCoord, ".csv"), delim = ",")
   GridName <- basename(FCoord)
-
+  rm(grid_polyg)
 } else {
   FCoord <- file.path(data_vc, paste0("SysGrid_500m_de_cote_", opt$region))
   GridName <- basename(FCoord)
@@ -124,7 +156,7 @@ BS <- 50
 BM <- 500
 BL <- 5000
 
-if (opt$region == "france_met"){
+if (opt$region == "france_met") {
   #  GIS Layers locations :
   folder_alan <- file.path(data_folder, "GIS", "ALAN")
   folder_vcf <- file.path(data_folder, "GIS", "VCF")
@@ -150,7 +182,7 @@ for (i in 1:length(listfun))
   source(listfun[i])
 }
 
-### ALAN ###
+###  ALAN ###
 print("ALAN")
 Coord_ALAN(
   points = FCoord,
@@ -160,7 +192,7 @@ Coord_ALAN(
   layers = folder_alan
 )
 
-### VCF ###
+###  VCF ###
 print("VCF")
 Coord_VCF(
   points = FCoord,
@@ -240,18 +272,18 @@ Coord_Meteo(
 loc_data <- file.path(loc, "data")
 
 
-if (exists("date_pred")){
+if (exists("date_pred")) {
   commande <- paste0("python group_obs.py --mode predict --date ", date_pred, " --loc ", loc_data)
-} else if (valid){
+} else if (valid) {
   espece <- as.character(svDialogs::dlgInput("Pour quelle espèces voulez-vous préparer les données ? (Barbar, Pippip...): ")$res)
   commande <- paste0("python group_obs.py --mode valid --sp ", espece, " --loc ", loc_data)
-}else{
+} else {
   espece <- as.character(svDialogs::dlgInput("Pour quelle espèces voulez-vous préparer les données ? (Barbar, Pippip...): ")$res)
   commande <- paste0("python group_obs.py --mode train --sp ", espece, " --loc ", loc_data)
 }
 
 
-if (getwd() != file.path(loc, "prep_data_chiros", "scripts")){
+if (getwd() != file.path(loc, "prep_data_chiros", "scripts")) {
   setwd("./prep_data_chiros/scripts")
 }
 
@@ -259,8 +291,8 @@ if (getwd() != file.path(loc, "prep_data_chiros", "scripts")){
 system(commande)
 
 # remplacer combine par appel à group_obs.py avec arguments (date, mode, espèce)
-#combineGIS_FR(
-  #points = FCoord,
-  #names_coord = Coord_Headers,
-  #layerlist = ListLayer
-#)
+# combineGIS_FR(
+# points = FCoord,
+# names_coord = Coord_Headers,
+# layerlist = ListLayer
+# )
