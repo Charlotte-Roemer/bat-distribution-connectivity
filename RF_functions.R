ef58f099748e8c7f419490b40ea77d9d4088f12c
thomas sévère <thomas.severe@mnhn.fr>
2025-02-18 14:05:54 +0100

    added print in moran’s function

--- RF_functions.R
+++ RF_functions.R
@@ -129,6 +129,7 @@ check_moran <- function(in_data, tested_variable) {
 
   # moran won’t work with no in_data in tested variable
   in_data <- subset(in_data, !is.na(in_data[, tested_variable]))
+  print(head(in_data[, tested_variable]))
 
   # in order to mesure moran we need to avoid duplicates in locations
   # by adding more or less 1m to the longitude randomly we won’t have such
