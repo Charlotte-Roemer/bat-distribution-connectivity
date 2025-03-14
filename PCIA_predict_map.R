42bcba150736a1043bbbf5d335cfc504f7c763ef
thomas sévère <thomas.severe@mnhn.fr>
2025-03-14 11:08:42 +0100

    pred variables path corrected in predict_map

--- PCIA_predict_map.R
+++ PCIA_predict_map.R
@@ -68,8 +68,7 @@ pred_data_file <- file.path(
   data_path,
   "observations",
   "pred_vars",
-  "data_pred",
-  paste0(opt$region, "_", opt$date_to_predict, ".csv")
+  paste0("data_pred", "_", opt$region, "_", opt$date_to_predict, ".csv")
 )
 
 pred_data <- data.table::fread(pred_data_file)
