5a26a37cedb7c096ce6c50df48c14248700118e3
thomas sévère <thomas.severe@mnhn.fr>
2025-06-02 15:11:30 +0200

    set datasaison$spgite to null

--- PCIA_RF_BMRE_evaluation.R
+++ PCIA_RF_BMRE_evaluation.R
@@ -395,6 +395,7 @@ for (i in seq_along(ListSp))
   DataSaison <- DataSaison |>
     drop_na(all_of(Prednames)) |> # deletes rows without predictor (outdated GI table)
     drop_na(nb_contacts) # deletes rows without contacts (people did not upload their data)
+  DataSaison$SpGite <- NULL
 
   if (opt$keep) {
     last_year <- max(DataSaison$SpYear)
