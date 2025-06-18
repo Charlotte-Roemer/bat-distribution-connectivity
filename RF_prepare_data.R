d9699ddf8721ea45136fa41426f9f4e3ee061deb
thomas sévère <thomas.severe@mnhn.fr>
2025-06-18 10:37:36 +0200

    def_classes factors acti_class again

--- RF_prepare_data.R
+++ RF_prepare_data.R
@@ -144,6 +144,6 @@ def_classes <- function(data) {
   data$acti_class[data$nb_contacts > quant[1] & data$nb_contacts <= quant[2]] <- "Moyen"
   data$acti_class[data$nb_contacts > quant[2] & data$nb_contacts <= quant[3]] <- "Fort"
   data$acti_class[data$nb_contacts > quant[3]] <- "TresFort"
-  # data$acti_class <- factor(data$acti_class, levels = c("NoAct", "Faible", "Moyen", "Fort", "TresFort"))
+  data$acti_class <- factor(data$acti_class, levels = c("NoAct", "Faible", "Moyen", "Fort", "TresFort"))
   data$acti_class
 }
