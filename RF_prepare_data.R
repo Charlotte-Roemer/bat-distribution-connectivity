f11e4a27f63207e5b4e5c21066441a28d77d1802
thomas sévère <thomas.severe@mnhn.fr>
2025-06-30 11:36:15 +0200

    vectorizing before facotrizing

--- RF_prepare_data.R
+++ RF_prepare_data.R
@@ -166,7 +166,7 @@ get_prednames <- function(df, prednames, response_var) {
   print("resp before factor")
   print(response)
   # response <- factor(response, levels = c("Faible", "Moyen", "Fort", "TresFort"))
-  response <- as.factor(response)
+  response <- as.factor(as.vector(response), levels = c("Faible", "Moyen", "Fort", "TresFort"))
   print("resp after factor")
   print(response)
   print("unique")
