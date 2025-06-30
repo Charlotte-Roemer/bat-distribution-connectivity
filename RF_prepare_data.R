fade8b698a35e7b0ec4340a95d28705e715f3745
thomas sévère <thomas.severe@mnhn.fr>
2025-06-30 14:31:44 +0200

    still

--- RF_prepare_data.R
+++ RF_prepare_data.R
@@ -164,9 +164,9 @@ get_prednames <- function(df, prednames, response_var) {
     dplyr::select(all_of(response_var))
 
   print("resp before factor")
-  print(is.factor(response))
-  # response <- factor(response, levels = c("Faible", "Moyen", "Fort", "TresFort"))
+  response <- factor(response, levels = c("Faible", "Moyen", "Fort", "TresFort"))
   # response <- as.factor(as.vector(response), levels = c("Faible", "Moyen", "Fort", "TresFort"))
+  print(is.factor(response))
   print("unique")
   print(unique(response))
 
