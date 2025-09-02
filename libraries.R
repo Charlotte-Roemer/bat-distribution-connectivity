00b55d8468993037d95fc9b6d7fa63cbe0dea2d1
thomas sévère <thomas.severe@mnhn.fr>
2025-09-02 12:46:09 +0200

    libraries.R added for installation

---
+++ libraries.R
@@ -0,0 +1,16 @@
+libs <- unique(c(
+  "sf",
+  "sfdep",
+  "caret",
+  "terra",
+  "gglot2",
+  "tidyr",
+  "dplyr",
+  "randomForest",
+  "optparse",
+  "stringr",
+  "readr",
+  "CAST",
+  "doParallel",
+  "exactextractr"
+))
