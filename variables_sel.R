eaa0513cd3183d14a60e3c94a00aad4ff0fc1402
thomas sévère <thomas.severe@mnhn.fr>
2025-10-29 15:10:06 +0100

    variables acp added to variables_sel.R

--- variables_sel.R
+++ variables_sel.R
@@ -1,9 +1,19 @@
 variables_a_exclure <- c("SpGite", "SpALAN_L", "SpAltiS", "SpAltiM", "SpAltiL", "SpPenM", "SpPenL", "Splat", "Splon")
 
 variables_indisp <- c(
-  SpBioC1, SpBioC10, SpBioC11, SpBioC12, SpBioC13, SpBioC14, SpBioC15,
-  SpBioC16, SpBioC17, SpBioC18, SpBioC19, SpBioC2, SpBioC3, SpBioC4, SpBioC5,
-  SpBioC6, SpBioC7, SpBioC8, SpBioC9, SpWD, SpWDp, SpHOCS2M, SpHOCS3M, SpHOCS4M,
-  SpHOCS6M, SpHOCS7M, SpHOCS9M, SpHOCS1M, SpHOCS5M, SpHOCS0M, SpHOCS8M, SpRo3M,
-  SpRo5L, SpRo_dist
+  "SpBioC1", "SpBioC10", "SpBioC11", "SpBioC12", "SpBioC13", "SpBioC14", "SpBioC15",
+  "SpBioC16", "SpBioC17", "SpBioC18", "SpBioC19", "SpBioC2", "SpBioC3", "SpBioC4", "SpBioC5",
+  "SpBioC6", "SpBioC7", "SpBioC8", "SpBioC9", "SpWD", "SpWDp", "SpHOCS2M", "SpHOCS3M", "SpHOCS4M",
+  "SpHOCS6M", "SpHOCS7M", "SpHOCS9M", "SpHOCS1M", "SpHOCS5M", "SpHOCS0M", "SpHOCS8M", "SpRo3M",
+  "SpRo5L", "SpRo_dist"
+)
+
+variables_acp <- c(
+  "SpBioC1", "SpBioC10", "SpBioC11", "SpBioC12", "SpBioC13",
+  "SpBioC14", "SpBioC15", "SpBioC16", "SpBioC17", "SpBioC18", "SpBioC19",
+  "SpBioC2", "SpBioC3", "SpBioC4", "SpBioC5", "SpBioC6", "SpBioC7", "SpBioC8",
+  "SpBioC9", "SpWD", "SpWDp", "SpHOCS2M", "SpHOCS3M", "SpHOCS4M", "SpHOCS6M",
+  "SpHOCS7M", "SpHOCS9M", "SpHOCS1M", "SpHOCS5M", "SpHOCS0M", "SpHOCS8M",
+  "SpRo3M", "SpRo5L", "SpRo_dist", "SpALAN_M", "SpNorM", "SpNorL", "SpEasM",
+  "SpEasL", "SpSumM", "SpSumL", "SpGro_M", "SpGro_L", "SpVCF_M", "SpVCF_L"
 )
