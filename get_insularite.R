12dbaa52127ea4eeb30544c1ec12680f9fd51d79
thomas sévère <thomas.severe@mnhn.fr>
2025-11-28 11:42:08 +0100

    trying distance with terra hoping for less memory usage and rasterize = true

--- get_insularite.R
+++ get_insularite.R
@@ -1,4 +1,3 @@
-library(sf)
 library(terra)
 source("variables.R")
 
@@ -8,23 +7,25 @@ source("variables.R")
 
 
 coastlines_file <- file.path(data_path, "GIS", "coastlines.gpkg")
-coastlines <- st_read(coastlines_file, layer = "eurasie")
+coastlines <- vect(coastlines_file, layer = "eurasie")
 
 france_file <- file.path(data_path, "GIS", "regions.gpkg")
-france <- st_read(france_file, layer = "france_met_uniques")
+france <- vect(france_file, layer = "france_met_uniques")
+france <- project(france, crs(coastlines))
 
-st_crs(france)
-france <- st_transform(france, st_crs(coastlines))
-st_crs(coastlines)
-
-
-nearest <- try(sf::st_nearest_feature(france, coastlines))
-
-insul_dist <- st_distance(france,
-  coastlines[nearest, ],
-  by_element = TRUE
-)
+insul_dist <- terra::distance(france, coastlines, rasterize = TRUE)
 
 france$insularite <- insul_dist
 
-st_write(france, france_file, layer = "france_met_insularite")
+writeVector(france, france_file, layer = "france_met_insularite")
+
+# nearest <- try(sf::st_nearest_feature(france, coastlines))
+#
+# insul_dist <- st_distance(france,
+#   coastlines[nearest, ],
+#   by_element = TRUE
+# )
+
+# france$insularite <- insul_dist
+
+# st_write(france, france_file, layer = "france_met_insularite")
