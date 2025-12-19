5ac73dbbb774b9bd18eb612d88a90a4a38fd3842
thomas sévère <thomas.severe@mnhn.fr>
2025-12-19 09:38:14 +0100

    removed meteo vars

--- variables_sel.R
+++ variables_sel.R
@@ -1,4 +1,4 @@
-variables_a_exclure <- c("SpSaison", "SpAltiM", "SpGite", "SpALAN_L", "SpAltiS", "SpAltiL", "SpPenM", "SpPenL", "Splat", "Splon")
+variables_a_exclure <- c("SpSaison", "SpAltiM", "SpGite", "SpALAN_L", "SpAltiS", "SpAltiL", "SpPenM", "SpPenL", "Splat", "Splon", "Spprecipitations", "Sptemp", "Spwind")
 # "SpAltiS","SpAltiM",
 variables_indisp <- c(
   "SpBioC1", "SpBioC10", "SpBioC11", "SpBioC12", "SpBioC13", "SpBioC14", "SpBioC15",
