af50b76556760b858eb90fd50119c9d67d1137ab
Thomas Sévère <tsevere@cca014.in2p3.fr>
2026-02-16 15:23:18 +0100

    error corrected in frances’route folder

--- Pipeline_GIS.R
+++ Pipeline_GIS.R
@@ -238,7 +238,7 @@ if (opt$region %in% c("idf", "france_met")) {
   folder_CLC <- file.path(data_folder, "GIS", "CLC")
   folder_OCS <- file.path(data_folder, "GIS", "OCS_OSO")
   folder_MOS <- file.path(data_folder, "GIS", "MOS")
-  folder_route <- file.path(data_folder, "GIS", "routes")
+  folder_route <- file.path(data_folder, "GIS", "ROUTE500")
   clim_norm_folder <- file.path(data_folder, "GIS", "CLIM_NORM")
   layer_wind_turbines <- file.path(data_folder, "GIS", "wind_turbines", "Mats_service_TOTAL.shp")
   bioclim_folder <- file.path(data_folder, "GIS", "chelsav2_bio")
