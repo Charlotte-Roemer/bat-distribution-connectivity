359698ffaf986ef9ea5f6b052c1dc94974dd5631
thomas sévère <thomas.severe@mnhn.fr>
2026-02-16 17:41:13 +0100

    ocs oso regex corrected

--- data_management/get_OCS_OSO.py
+++ data_management/get_OCS_OSO.py
@@ -1,4 +1,3 @@
-# %%
 from pygeodes import Geodes
 from pygeodes.utils.formatting import format_items
 from pygeodes import Config
@@ -11,16 +10,16 @@ geodes = Geodes()
 config_file = "data_management/config_geodes.json"
 conf = Config.from_file(config_file)
 
-# geodes.set_conf(conf)
-#
-#
-# bbox = [-13, 34, 40, 71]
-#
-# items, dataframe = geodes.search_items(
-#     bbox=bbox, collections=["THEIA_OSO_RASTER_L3B"], get_all=True)
-#
-# for item in items:
-#     item.download_archive()
+geodes.set_conf(conf)
+
+
+bbox = [-13, 34, 40, 71]
+
+items, dataframe = geodes.search_items(
+    bbox=bbox, collections=["THEIA_OSO_RASTER_L3B"], get_all=True)
+
+for item in items:
+    item.download_archive()
 
 with open(config_file, 'r') as file:
     data = json.load(file)
@@ -32,6 +31,6 @@ for file in files:
     print(file)
     with zipfile.ZipFile(file, 'r')as zipped_file:
         for info in zipped_file.infolist():
-            if re.match(r'^DATA/OCS_[0-9]{4}_LZW.tif', info.filename):
+            if re.match(r'^DATA/OCS_[0-9]{4}.*\.tif', info.filename):
                 print(info.filename)
                 zipped_file.extract(info, path=dir)
