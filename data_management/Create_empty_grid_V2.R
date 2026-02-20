afa2d2e576f16f8ea6dc099c2fd6a942bbd6de1d
thomas sévère <thomas.severe@mnhn.fr>
2026-02-20 10:48:33 +0100

    changed location of writing sysgrid to match extraction

--- data_management/Create_empty_grid_V2.R
+++ data_management/Create_empty_grid_V2.R
@@ -51,8 +51,6 @@ coords$ID <- 1:nrow(coords)
 # for (i in seq_along(names1)) {
 st_write(
   coords,
-  file.path(data_loc, "GIS", paste0(GridName, ".csv"))
+  file.path(data_loc, "observations", "data_pred", paste0(GridName, ".csv"))
 )
 # }
-
-data_loc
