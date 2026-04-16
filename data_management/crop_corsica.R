95b1d7fd68646ddd51e76dc8ab5dc3514ce0286a
Charlotte-Roemer <62648588+Charlotte-Roemer@users.noreply.github.com>
2026-04-16 16:28:55 +0200

    added sysgrid

--- data_management/crop_corsica.R
+++ data_management/crop_corsica.R
@@ -15,16 +15,25 @@ data_train = read_delim(data_train_path)
 data_pred_path <- file.path(data_path, "observations", "pred_vars", "data_pred_france_met.csv")
 data_pred = read_delim(data_pred_path)
 
+# Load SysGrid
+sysgrid_path <- file.path(data_path, "observations", "SysGrid_500m_de_cote_france_met.csv")
+sysgrid = read_delim(sysgrid_path)
+
 # Crop Corsica
 data_train_sf = st_as_sf(data_train, # convert to sf
     coords = c("X", "Y"), crs=4326, remove=FALSE)
 data_pred_sf = st_as_sf(data_pred, # convert to sf
     coords = c("X", "Y"), crs=4326, remove=FALSE)
+sysgrid_sf = st_as_sf(sysgrid, # convert to sf
+    coords = c("X", "Y"), crs=4326, remove=FALSE)
 data_train_corsica_sf <- st_intersection(data_train_sf, GIS_limits)
 data_pred_corsica_sf <- st_intersection(data_pred_sf, GIS_limits)
+sysgrid_corsica_sf <- st_intersection(sysgrid_sf, GIS_limits)
 data_train_corsica = st_drop_geometry(data_train_corsica_sf)
 data_pred_corsica = st_drop_geometry(data_pred_corsica_sf)
+sysgrid_corsica = st_drop_geometry(sysgrid_corsica_sf)
 
 # Save
 write_csv(data_train_corsica, paste0(data_path, "/observations/obs_vars/data_train_corsica.csv"))
 write_csv(data_pred_corsica, paste0(data_path, "/observations/pred_vars/data_pred_corsica.csv"))
+write_csv(sysgrid_corsica, paste0(data_path, "/observations/pred_vars/SysGrid_500m_de_cote_corsica.csv"))
