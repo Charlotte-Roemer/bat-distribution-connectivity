5be33dc64302d30d9d281be505d30e134e95299a
Charlotte-Roemer <62648588+Charlotte-Roemer@users.noreply.github.com>
2026-06-03 21:06:54 +0200

    Update f_Coord_Ocsol_europe.R

--- data_management/f_Coord/europe/f_Coord_Ocsol_europe.R
+++ data_management/f_Coord/europe/f_Coord_Ocsol_europe.R
@@ -39,7 +39,7 @@ Coord_Land_Cover <- function(points, names_coord, bs, bm, bl, layer) {
       sf::st_as_sf(coords = c("X", "Y"), crs = 4326, remove = FALSE)
 
     OccSL_L3035 <- OccSL %>%
-      sf::st_transform(2154)
+      sf::st_transform(3035)
   }
 
   OccSL_L3035$year <- sapply(strsplit(OccSL_L3035$Nuit, "-"), "[", 1)
@@ -77,38 +77,23 @@ Coord_Land_Cover <- function(points, names_coord, bs, bm, bl, layer) {
   #OCS <- terra::project(OCS, "epsg:3035")
   #print("Raster reprojected")
 
-  # prendre seulement 1000 points pour le test
-  #n_test <- min(1000, nrow(OccSL_L3035))
-  #OccSL_L3035 <- OccSL_L3035[1:n_test, ] # SUPPRIMER CETTE LIGNE QUAND J'AURAIS FINI LE TEST !!!!
+  # # Operation to crop ESA worldcover to correspond to the zone of points 
+  # # before reprojecting ESA to epsg:3035 because to lead to OOM killing
+  # zone_3035 <- sf::st_transform(zone, 3035)
+  # zone_vect <- terra::vect(zone_3035)
+  # zone_vect <- terra::project(zone_vect, terra::crs(OCS))
+  # print("Zone reprojected")
+  # OCS_crop <- terra::crop(OCS, zone_vect) # crop
+  # #OCS_crop <- terra::mask(OCS_crop, zone_vect) # puts values to NA if they are not in the polygon formed by BL
+  # OCS_crop_3035 <- terra::project( # reprojection to epsg:3035
+  # OCS_crop,
+  # "epsg:3035",
+  # method = "near"
+  # )
 
-  # Operation to crop ESA worldcover to correspond to the zone of points 
-  # before reprojecting ESA to epsg:3035 because to lead to OOM killing
-  zone_3035 <- sf::st_transform(zone, 3035)
-  zone_vect <- terra::vect(zone_3035)
-  zone_vect <- terra::project(zone_vect, terra::crs(OCS))
-  print("Zone reprojected")
-  OCS_crop <- terra::crop(OCS, zone_vect) # crop
-  #OCS_crop <- terra::mask(OCS_crop, zone_vect) # puts values to NA if they are not in the polygon formed by BL
-  OCS_crop_3035 <- terra::project( # reprojection to epsg:3035
-  OCS_crop,
-  "epsg:3035",
-  method = "near"
-  )
-
-  rm(OCS, OCS_crop)
+  #rm(OCS, OCS_crop)
 
   print("Raster reprojected")
-
-  # print("Creating 100m raster")
-  # t_agg <- system.time({
-  # OCS_100m <- terra::aggregate(
-  #  OCS_crop_3035,
-  #  fact = 10,
-  #  fun = modal,
-  #  na.rm = TRUE
-  # )
-  # })
-  # print(t_agg)
 
   # create a buffer around the points
   tableau_BM <- sf::st_buffer(OccSL_L3035, bm)
@@ -120,10 +105,23 @@ Coord_Land_Cover <- function(points, names_coord, bs, bm, bl, layer) {
 
   print("Buffers created")
 
+  # reproject buffers to raster's CRS 
+  tableau_BM <- sf::st_transform(
+    tableau_BM,
+    terra::crs(OCS)
+  )
+
+  tableau_BL <- sf::st_transform(
+    tableau_BL,
+    terra::crs(OCS)
+  )
+
+cat("Buffers reprojected\n")
+
   # Extract values in medium buffer
   print("Medium buffer")
   t_medium <- system.time({
-  landcov_fracs_Medium <- exactextractr::exact_extract(OCS_crop_3035, tableau_BM, function(df) {
+  landcov_fracs_Medium <- exactextractr::exact_extract(OCS, tableau_BM, function(df) {
     df %>%
       dplyr::mutate(frac_total = coverage_fraction / sum(coverage_fraction)) %>%
       dplyr::group_by(FID, value) %>%
@@ -137,7 +135,7 @@ Coord_Land_Cover <- function(points, names_coord, bs, bm, bl, layer) {
   # Extract values in large buffer
   print("Large buffer 10m")
   t_large10 <- system.time({
-  landcov_fracs_Large <- exactextractr::exact_extract(OCS_crop_3035, tableau_BL, function(df) {
+  landcov_fracs_Large <- exactextractr::exact_extract(OCS, tableau_BL, function(df) {
     df %>%
       dplyr::mutate(frac_total = coverage_fraction / sum(coverage_fraction)) %>%
       dplyr::group_by(FID, value) %>%
@@ -174,7 +172,7 @@ Coord_Land_Cover <- function(points, names_coord, bs, bm, bl, layer) {
   # print(t_large100)
 
   # Append large buffer list
-  rm(OCS_crop_3035, tableau_BL)
+  rm(OCS, tableau_BL)
 
   # Pivot tibbles and rename columns
   landcov_fracs_Medium_pivot <- landcov_fracs_Medium %>%
@@ -191,43 +189,43 @@ Coord_Land_Cover <- function(points, names_coord, bs, bm, bl, layer) {
 
   rm(landcov_fracs_Large)
 
-  landcov_fracs_Large100_pivot <- landcov_fracs_Large_100m %>%
-  tidyr::pivot_wider(
-    names_from = value,
-    values_from = freq
-  ) %>%
-  replace(is.na(.), 0)
+  # landcov_fracs_Large100_pivot <- landcov_fracs_Large_100m %>%
+  # tidyr::pivot_wider(
+  #   names_from = value,
+  #   values_from = freq
+  # ) %>%
+  # replace(is.na(.), 0)
 
-  rm(landcov_fracs_Large_100m)
+  # rm(landcov_fracs_Large_100m)
 
-  print("COMPARAISON")
+  # print("COMPARAISON")
 
-  comp <- dplyr::inner_join(
-  landcov_fracs_Large10_pivot,
-  landcov_fracs_Large100_pivot,
-  by = "FID",
-  suffix = c("_10m", "_100m")
-  )
+  # comp <- dplyr::inner_join(
+  # landcov_fracs_Large10_pivot,
+  # landcov_fracs_Large100_pivot,
+  # by = "FID",
+  # suffix = c("_10m", "_100m")
+  # )
 
-  classes <- setdiff(
-  names(landcov_fracs_Large10_pivot),
-  "FID"
-  )
+  # classes <- setdiff(
+  # names(landcov_fracs_Large10_pivot),
+  # "FID"
+  # )
 
-  for(cl in classes){
+  # for(cl in classes){
 
-  cat("\n", cl, "\n")
+  # cat("\n", cl, "\n")
 
-  print(
-    cor(
-      comp[[paste0(cl, "_10m")]],
-      comp[[paste0(cl, "_100m")]],
-      use = "complete.obs"
-    )
-  )
-  }
+  # print(
+  #   cor(
+  #     comp[[paste0(cl, "_10m")]],
+  #     comp[[paste0(cl, "_100m")]],
+  #     use = "complete.obs"
+  #   )
+  # )
+  # }
 
-  stop("FIN COMPARAISON")
+  # stop("FIN COMPARAISON")
 
   HabufPropT_Tot <- dplyr::inner_join(landcov_fracs_Medium_pivot,
     landcov_fracs_Large_pivot,
