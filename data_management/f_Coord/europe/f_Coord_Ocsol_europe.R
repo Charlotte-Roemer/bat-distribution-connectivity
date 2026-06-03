b53f617c9ee4bc5a006623ee5e7283081a2a938e
Charlotte-Roemer <62648588+Charlotte-Roemer@users.noreply.github.com>
2026-06-03 19:39:23 +0200

    try directly with zone instead of buffers

--- data_management/f_Coord/europe/f_Coord_Ocsol_europe.R
+++ data_management/f_Coord/europe/f_Coord_Ocsol_europe.R
@@ -81,8 +81,6 @@ Coord_Land_Cover <- function(points, names_coord, bs, bm, bl, layer) {
   #n_test <- min(1000, nrow(OccSL_L3035))
   #OccSL_L3035 <- OccSL_L3035[1:n_test, ] # SUPPRIMER CETTE LIGNE QUAND J'AURAIS FINI LE TEST !!!!
 
-  print("Creating buffers")
-
   # create a buffer around the points
   tableau_BM <- sf::st_buffer(OccSL_L3035, bm)
   tableau_BL <- sf::st_buffer(OccSL_L3035, bl)
@@ -91,11 +89,14 @@ Coord_Land_Cover <- function(points, names_coord, bs, bm, bl, layer) {
 
   rm(OccSL_L3035)
 
+  print("Buffers created")
+
   # Operation to crop ESA worldcover to correspond to the zone of points 
   # before reprojecting ESA to epsg:3035 because to lead to OOM killing
-  zone <- sf::st_union(tableau_BL)
-  zone_vect <- terra::vect(zone)
+  zone_3035 <- sf::st_transform(zone, 3035)
+  zone_vect <- terra::vect(zone_3035)
   zone_vect <- terra::project(zone_vect, terra::crs(OCS))
+  print("Zone reprojected")
   OCS_crop <- terra::crop(OCS, zone_vect) # crop
   OCS_crop <- terra::mask(OCS_crop, zone_vect) # puts values to NA if they are not in the polygon formed by BL
   OCS_crop_3035 <- terra::project( # reprojection to epsg:3035
