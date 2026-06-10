dc4bcf4a5380656d6952db9cb86001ea8895ed2b
Charlotte-Roemer <62648588+Charlotte-Roemer@users.noreply.github.com>
2026-06-10 16:31:53 +0200

    Update group_vars.R

--- data_management/group_vars.R
+++ data_management/group_vars.R
@@ -20,7 +20,7 @@ print(mode_name)
 region <- commandArgs(trailingOnly = TRUE)[2L]
 
 fichiers <- list.files(folder,
-  full.names = TRUE, pattern = region
+  full.names = TRUE, pattern = paste0("loc_train_", region)
 )
 
 fichiers
@@ -61,10 +61,8 @@ for (df in dataframes) {
   print(names(df))
   if ("Nuit" %in% colnames(df)) {
     df <- unique(df)
-    print("A")
     print(base$Nuit)
     base$Nuit <- df$Nuit[1] # A enlever si ça marche pas
-    print("B")
     print(base$Nuit)
     by <- c("X", "Y", "Nuit")
     base <- dplyr::left_join(base, df, by = by)
