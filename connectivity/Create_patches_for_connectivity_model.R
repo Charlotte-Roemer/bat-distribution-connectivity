982b5cddca95051e7cc9109cbd4f6471c51b7e12
Charlotte-Roemer <62648588+Charlotte-Roemer@users.noreply.github.com>
2026-07-02 11:18:35 +0200

    Update Create_patches_for_connectivity_model.R

--- connectivity/Create_patches_for_connectivity_model.R
+++ connectivity/Create_patches_for_connectivity_model.R
@@ -93,7 +93,7 @@ Clump_function <- function(Raster_sub)
 
 # Load files
 print("Load files")
-print(paste0("^", Name, ".*.", opt$species, ".*.", opt$region, ".*", predictions.tif$"))
+print(paste0("^", Name, ".*.", opt$species, ".*.", opt$region, ".*.", predictions.tif$"))
 list_file <- list.files(Directory, recursive=TRUE, pattern=paste0("^", Name, ".*.", opt$species, ".*.", opt$region, ".*.", predictions.tif$"))
 
 ls2 = paste(Directory, list_file, sep="/")
